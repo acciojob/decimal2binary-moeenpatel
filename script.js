bdbe6170a829906c0e5c6b02793221e507a64564
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-07-26 21:21:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 function decimalToBinary(num) {
-  // Write your code here
-	let arr=[];
-	let r;
-	let i=0;
-	while(num!=0){
-		r=num%2;//0 1 0 1
-		arr[i]+=r;//0101 
-		num=num/2;//0
-		i++;
-	}
-	arr.reverse();
-	return arr;
+  //Write you code here
+	return num.toString(2);
+  
 }
 
 window.decimalToBinary = decimalToBinary;
